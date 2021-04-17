0e236613db995e48147ec1c31140a9a68db0a4bb
Mason Tianyi Chen <39594659+masontchen@users.noreply.github.com>
2021-04-17 13:04:17 -0400

    Create q1_network.py

---
+++ tests/q1_network.py
@@ -0,0 +1,11 @@
+import codecs
+import re
+import glob
+
+if __name__ == "__main__":
+    filename = glob.glob('./*.ipynb')
+    f = codecs.open(''.join(filename), "r", "utf-8").read()
+    command1 = re.compile("Sequential")
+    command2 = re.compile("Conv2D")
+    command3 = re.compile("MaxPooling2D")
+    assert len(command1.findall(f)) + len(command2.findall(f)) + len(command3.findall(f)) > 2
